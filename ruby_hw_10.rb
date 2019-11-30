21aa9fd8961dfa82283684615a87b88ae92960ef
yerlanmad <studyworld.m@gmail.com>
2019-11-30 12:24:52 +0600

    finishing task

--- ruby_hw_10.rb
+++ ruby_hw_10.rb
@@ -1,12 +1,30 @@
-def perform(str, array = [])
+def perform(insturction, array = [])
   if block_given?
-    result = yield(str, array[])
+    puts result = yield(insturction, array)
   else
     puts "Please give me instructions"
   end
 end
 
-perform("find words", ["Hello, Mike", "My name is", "What is the beatuful day"]) do |str, array|
-  if str == "find words"
-    array.map! { |el| el.count(" ") + 1 }
-    
+perform("find words", ["Hello, Mike", "My name is", "What is the beatuful day"]) do |insturction, array|
+
+  if insturction == "find words" && array.all? { |el| el.is_a? String }
+    el_length = array.map { |el| el.split(/\s+/).length }
+    array[el_length.index(el_length.max)]
+  else
+    error = "Wrong instructions"
+  end
+
+end
+
+perform("parse numbers", [9, 1, 5, 7]) do |insturction, array|
+
+  if insturction == "parse numbers" && array.all? { |el| el.is_a? Integer }
+    hash_of_numbers = {}
+    array.each_with_index { |el, index| hash_of_numbers[index + 1] = el }
+    hash_of_numbers
+  else
+    error = "Wrong instructions"
+  end
+
+end
